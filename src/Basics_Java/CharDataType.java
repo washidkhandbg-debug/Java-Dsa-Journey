1598b736db80f6ee1992cf019fecb15aaf8adf0c
washidkhan-dev <washidkhandbg@gmail.com>
2026-04-21 10:40:45 -0700

    Java basics program

--- src/Basics_Java/CharDataType.java
+++ src/Basics_Java/CharDataType.java
@@ -2,6 +2,10 @@ package Basics_Java;
 
 public class CharDataType {
     public static void main(String[] args) {
+        int rollno = 64;
+        System.out.println(rollno);
+        float marks = 98.67f;
+        System.out.println(marks);
         char Washid = 's';
         System.out.println(Washid);
         char afnan = 'e';
--- src/Basics_Java/HelloWashid.java
+++ src/Basics_Java/HelloWashid.java
@@ -1,7 +1,10 @@
 package Basics_Java;
 
-public class HelloWashid {
+
+
+public class HelloWashid{
     public static void main(String[] args) {
+
         System.out.print("HELLO WASHID");
     }
 }
